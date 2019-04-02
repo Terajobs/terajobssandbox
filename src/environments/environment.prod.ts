250156bb0dfd48967a052e748fa70fe92faf3e12
poenix111 <nazazuke@gmail.com>
2019-04-02 08:10:08 -0600

    Agregue la clave de firebase al enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+    firebase: {
+    apiKey: 'AIzaSyCiXe6QvA7yenOMe9FpDNVDU476dcXsMUA',
+    authDomain: 'terajobsopsandbox.firebaseapp.com',
+    databaseURL: 'https://terajobsopsandbox.firebaseio.com',
+    projectId: 'terajobsopsandbox',
+    storageBucket: 'terajobsopsandbox.appspot.com',
+    messagingSenderId: '852753087316'
+  }
 };
