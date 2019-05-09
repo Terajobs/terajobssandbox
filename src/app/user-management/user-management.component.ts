0cfb7a01c719bf69da6b9195e1262cd70af2a87c
Hugo Valencia <hugo.valencia@inbest.cloud>
2019-05-09 06:05:46 -0500

    se agregaron más comentarios al codigo

--- src/app/user-management/user-management.component.ts
+++ src/app/user-management/user-management.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FirebaseService } from '../services/firebase/firebase.service';
 
+//componente para el reset de la contraseña
+//el menaje del correo se modifica desde la consola de Firebase
+//en el archivo del servicio de firebase se ha agregado codigo para manejar el envio del correo
+// importante!! falta terminar este componente, por eso no se encuentra en la documentación
 @Component({
   selector: 'app-user-management',
   templateUrl: './user-management.component.html',
